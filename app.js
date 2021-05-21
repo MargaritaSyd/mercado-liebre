2ba229cf9180a9179573a200817ae42ea7b32bb1
MargaritaSyd <maggie@leles-MacBook-Air.local>
2021-05-21 16:02:00 -0300

    creando M Liebre

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require ('path');
+
+app.listen(3001, () => {
+    console.log("Servidor corriendo")
+});
+
+//app.get('/' , (req,res) => {
+//    res.send('Hola Mundo!');
+//})
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.resolve(__dirname , './view/home.html'))
+});
+
+app.use(express.static(path.resolve(__dirname , './public')))
