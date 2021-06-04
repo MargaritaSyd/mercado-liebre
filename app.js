58a4ea5f60a30b9157556a8954ec843493e5ff10
MargaritaSyd <margarita.syddall@gmail.com>
2021-06-04 12:45:10 -0300

    estilo y html

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require ('path');
 
-app.listen(3002, () => {
+app.listen(3000, () => {
     console.log("Servidor corriendo")
 });
 
@@ -12,6 +12,14 @@ app.listen(3002, () => {
 
 app.get('/' , (req,res) => {
     res.sendFile(path.resolve(__dirname , './view/home.html'))
+});
+
+app.get('/register' , (req,res) => {
+    res.sendFile(path.resolve(__dirname , './view/register.html'))
+});
+
+app.get('/login' , (req,res) => {
+    res.sendFile(path.resolve(__dirname , './view/login.html'))
 });
 
 app.use(express.static(path.resolve(__dirname , './public')))
